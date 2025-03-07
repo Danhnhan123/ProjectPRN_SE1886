377ea8fb0c3b93d285858f8f853e94efa0e43c65
nhandndhe180152 <nhandndhe180152@fpt.edu.vn>
2025-03-07 14:51:48 +0700

    Add project files.

---
+++ Models/PrnProjectContext.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ProjectPRN_SE1886.Models;
+
+public partial class PrnProjectContext : DbContext
+{
+    public PrnProjectContext()
+    {
+    }
+
+    public PrnProjectContext(DbContextOptions<PrnProjectContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Household> Households { get; set; }
+
+    public virtual DbSet<HouseholdMember> HouseholdMembers { get; set; }
+
+    public virtual DbSet<Log> Logs { get; set; }
+
+    public virtual DbSet<Notification> Notifications { get; set; }
+
+    public virtual DbSet<Registration> Registrations { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Directory.GetCurrentDirectory());
+        builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        var configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("Default"));
+    }
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseSqlServer("Server= dunghoa\\SQLEXPRESS;uid=sa;password=123;database=PRN_Project;Encrypt=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Household>(entity =>
+        {
+            entity.HasKey(e => e.HouseholdId).HasName("PK__Househol__1453D6ECCCB44588");
+
+            entity.Property(e => e.HouseholdId).HasColumnName("HouseholdID");
+            entity.Property(e => e.Address).HasColumnType("text");
+            entity.Property(e => e.CreatedDate).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.HeadOfHouseholdId).HasColumnName("HeadOfHouseholdID");
+
+            entity.HasOne(d => d.HeadOfHousehold).WithMany(p => p.Households)
+                .HasForeignKey(d => d.HeadOfHouseholdId)
+                .HasConstraintName("FK__Household__HeadO__4E88ABD4");
+        });
+
+        modelBuilder.Entity<HouseholdMember>(entity =>
+        {
+            entity.HasKey(e => e.MemberId).HasName("PK__Househol__0CF04B382F912BAC");
+
+            entity.Property(e => e.MemberId).HasColumnName("MemberID");
+            entity.Property(e => e.HouseholdId).HasColumnName("HouseholdID");
+            entity.Property(e => e.Relationship)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Household).WithMany(p => p.HouseholdMembers)
+                .HasForeignKey(d => d.HouseholdId)
+                .HasConstraintName("FK__Household__House__5812160E");
+
+            entity.HasOne(d => d.User).WithMany(p => p.HouseholdMembers)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Household__UserI__59063A47");
+        });
+
+        modelBuilder.Entity<Log>(entity =>
+        {
+            entity.HasKey(e => e.LogId).HasName("PK__Logs__5E5499A8D017A67B");
+
+            entity.Property(e => e.LogId).HasColumnName("LogID");
+            entity.Property(e => e.Action)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Timestamp)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Logs)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Logs__UserID__619B8048");
+        });
+
+        modelBuilder.Entity<Notification>(entity =>
+        {
+            entity.HasKey(e => e.NotificationId).HasName("PK__Notifica__20CF2E32189EFB32");
+
+            entity.Property(e => e.NotificationId).HasColumnName("NotificationID");
+            entity.Property(e => e.IsRead).HasDefaultValue(false);
+            entity.Property(e => e.Message).HasColumnType("text");
+            entity.Property(e => e.SentDate)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("datetime");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Notifications)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Notificat__UserI__5DCAEF64");
+        });
+
+        modelBuilder.Entity<Registration>(entity =>
+        {
+            entity.HasKey(e => e.RegistrationId).HasName("PK__Registra__6EF58830079970E2");
+
+            entity.Property(e => e.RegistrationId).HasColumnName("RegistrationID");
+            entity.Property(e => e.Comments).HasColumnType("text");
+            entity.Property(e => e.RegistrationType)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Status)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasDefaultValue("Pending");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.ApprovedByNavigation).WithMany(p => p.RegistrationApprovedByNavigations)
+                .HasForeignKey(d => d.ApprovedBy)
+                .HasConstraintName("FK__Registrat__Appro__5535A963");
+
+            entity.HasOne(d => d.User).WithMany(p => p.RegistrationUsers)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK__Registrat__UserI__5441852A");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CCAC2D532683");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534967217D7").IsUnique();
+
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+            entity.Property(e => e.Address).HasColumnType("text");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Password)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
