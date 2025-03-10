3cb920a97beb4c3f09f5f9c561ee0db06db9a303
nhandndhe180152 <nhandndhe180152@fpt.edu.vn>
2025-03-10 07:18:40 +0700

    fix mainwindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ProjectPRN_SE1886.Models;
 
 namespace ProjectPRN_SE1886
 {
@@ -16,9 +18,43 @@ namespace ProjectPRN_SE1886
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            LoadHouseholds();
+        }
+
+        private void LoadHouseholds()
+        {
+            var households = householdDAO.GetHouseholds();
+            DataGridResidents.ItemsSource = households;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement search logic here
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement add logic here
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement edit logic here
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement delete logic here
+        }
+
+        private void SortButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Implement sort logic here
         }
     }
 }
